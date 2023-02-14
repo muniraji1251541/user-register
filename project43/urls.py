f8b588a9ea18768a0a8f9f85e1e14465070a917f
muniraji1251541 <muniraji1251541@gmail.com>
2023-02-14 16:18:46 +0530

    first commit

--- project43/urls.py
+++ project43/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',home,name='home'),
     path('register/',register,name='register'),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
